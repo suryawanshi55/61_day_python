f3e094100c312e6a673c66e611b639cca6f82bab
suryawanshi55 <107236171+suryawanshi55@users.noreply.github.com>
2022-08-16 21:25:03 +0530

    Add files via upload

---
+++ nested_for.py
@@ -0,0 +1,11 @@
+# a1=[
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9,10]
+# ]
+# print(a1)             # [[1, 2, 3], [4, 5, 6], [7, 8, 9, 10]]
+# print(a1[0])            # [1, 2, 3]
+# print(a1[0][0])             1
+# for n in a1:
+#     for m in n:
+#         print(m)        #print vertically from 1 to 10
